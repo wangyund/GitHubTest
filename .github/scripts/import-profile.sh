46de707c43de1896d7da5b80fdfdf8925e7b1d61
王允顶 <42368165+wangyund@users.noreply.github.com>
2020-11-24 16:47:20 +0800

    Update import-profile.sh

--- .github/scripts/import-profile.sh
+++ .github/scripts/import-profile.sh
@@ -1,1 +1,4 @@
+set -euo pipefail
 
+mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+echo "$PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/profile.mobileprovision
