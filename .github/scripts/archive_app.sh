386dbb9ce2a6f02a8ae8bf2c096d8c5fae59723d
王允顶 <42368165+wangyund@users.noreply.github.com>
2020-11-24 10:00:16 +0800

    Update archive_app.sh

--- .github/scripts/archive_app.sh
+++ .github/scripts/archive_app.sh
@@ -1,3 +1,8 @@
 set -eo pipefail
 
-xcodebuild -workspace GitHubTest.xcworkspace -scheme GitHubTest -configuration Release clean
+# 获取当前路径
+workdir=$(pwd)
+## cd  到程序路径
+cd $workdir
+
+xcodebuild -workspace GitHubTest.xcodeproj -scheme GitHubTest -configuration Release clean
